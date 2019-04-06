854a53ed29baf3bdc50248515eb91a955766575e
Amuzdalifa <a.muzdalifa@gmail.com>
2019-04-06 10:14:35 +0700

    Fix functionality

--- wordembedder.py
+++ wordembedder.py
@@ -11,34 +11,45 @@ class Wordembedder:
 	X = Wordembedder(docs).embedding_matrix
 	
 	docs = list of sentence in document
-	example:
-	docs = ['this is sentence 1',
-		'this is sentence 2',
-		'this is last sentence'
-		]
 	"""
-	def __init__(self, docs):
-		filename = 'GoogleNews-vectors-negative300.bin'
-		self._wordvector = gen.models.KeyedVectors.load_word2vec_format(filename, binary=True)
-		self._embedding_matrix = self.createEmbeddingMatrix(docs)
+	def __init__(self, docs, filename=None):
+		self._encoded_docs = self.encodeDocs(docs)
+		if None:
+			self._embedding_matrix = self.createEmbeddingMatrix()
+		else:
+			self._embedding_matrix = self.loadFromFile(filename)
 
 	@property
-	def wordvector(self):
-		return self._wordvector
+	def encoded_docs(self):
+		return self._encoded_docs
 
 	@property
 	def embedding_matrix(self):
 		return self._embedding_matrix
 
-	def createEmbeddingMatrix(self, docs):
+	@property
+	def vocab_size(self):
+		return self._vocab_size
+
+	@property
+	def max_length(self):
+		return self._max_length
+
+	def encodeDocs(self, docs):
 		t = Tokenizer(filters='!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~', lower=True, split=None)
 		t.fit_on_texts(docs)
-		vocab_size = len(t.word_index) + 1
+		self._vocab_size = len(t.word_index) + 1
 		encoded_docs = t.texts_to_sequences(docs)
-		max_length = max([len(encoded_sent) for encoded_sent in encoded_docs])
-		padded_docs = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
+		self._max_length = max([len(encoded_sent) for encoded_sent in encoded_docs])
+		padded_docs = pad_sequences(encoded_docs, maxlen=self._max_length, padding='post')
+
+		return padded_docs
+
+	def createEmbeddingMatrix(self):
+		filename = 'GoogleNews-vectors-negative300.bin'
+		wordvector = gen.models.KeyedVectors.load_word2vec_format(filename, binary=True)
 		embedding_dim = 300
-		embedding_matrix = np.zeros((vocab_size, embedding_dim))
+		embedding_matrix = np.zeros((self._vocab_size, embedding_dim))
 		for word, i in t.word_index.items():
 		    try:
 		        embedding_vector = wordvector[word] #wordvector = model dari Google
@@ -48,6 +59,11 @@ class Wordembedder:
 
 		return embedding_matrix
 
+	def saveToFile(self, filename):
+		np.save(filename, arr=self._embedding_matrix)
+
+	def loadFromFile(self, path):
+		self._embedding_matrix = np.load(path)
 
 	
 
